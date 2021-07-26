87d077da58b03a983b8cf15d187c7141dfa3b3e3
alex2116 <reingeld116@gmail.com>
2021-07-26 17:03:11 +0800

    feat: add res finish time function

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const time = Date.now()
-var date = new Date(time)
 
-app.use(function (req, res, next) {
-  console.log(date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + ' | Get from '+ req.originalUrl)
+// const reqTime = (req, res, next) => {
+//   const time = Date.now()
+//   const date = new Date(time) //轉換成object?
+//   console.log(date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + ' | Get from ' + req.originalUrl)
+//   next()
+// }
+
+app.use(function (req, res, next) { 
+  const time = Date.now()
+  const startTime = new Date(time) //轉換成object?  
+  const detail = startTime.getFullYear() + "/" + (startTime.getMonth() + 1) + "/" + startTime.getDate() + " " + startTime.getHours() + ":" + startTime.getMinutes() + ":" + startTime.getSeconds() + ' | Get from ' + req.originalUrl
+
+  console.log(`接收時間:${detail}`)
+
+  res.on('finish', () => {
+    const endTime = Date.now()
+    const finishTime = new Date(endTime) 
+    const finishTimeDetail = finishTime.getFullYear() + "/" + (finishTime.getMonth() + 1) + "/" + finishTime.getDate() + " " + finishTime.getHours() + ":" + finishTime.getMinutes() + ":" + finishTime.getSeconds() + ' | Get from ' + req.originalUrl
+
+    const duration = finishTime - time
+    const durationSecond = new Date(duration)
+    console.log(`結束時間:${finishTimeDetail} | 共花了 ${durationSecond.getSeconds()}秒`)    
+  })
   next()
 })
 
 app.get('/', (req, res) => {
-  res.send('列出全部 Todo')
+  setTimeout(() => {
+    res.send('列出全部 Todo')
+  },8000)
 })
 
 app.get('/new', (req, res) => {
@@ -24,6 +45,7 @@ app.get('/:id', (req, res) => {
 app.post('/', (req, res) => {
   res.send('新增一筆  Todo')
 })
+
 
 // function logOriginalUrl(req, res, next) {
 //   console.log('Request URL:', req.originalUrl)
